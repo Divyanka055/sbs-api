ba5484fa7050bf88f71f3b01edcaab72c1504f2f
Divyanka kumari <divyankachandra82@gmail.com>
2026-04-09 18:44:51 -0700

    changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const mongoose = require('mongoose')
 
 const connectWithDatabase = async() => {
     try {
-        await mongoose.connect(process.env.B_URL)
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log("connect to database")
     }
     catch(err){
